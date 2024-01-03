f681b7e3183b2a053594ab262edca3a5fdcb2254
Leandro Amaral <138171959+Leanddo@users.noreply.github.com>
2024-01-03 22:08:41 -0100

    Finished project

---
+++ script.js
@@ -0,0 +1,14 @@
+function check() {
+  let switchinp = document.getElementById("switch-input").checked;
+
+  if (switchinp == true) {
+    document.getElementById("circle").style.left = "140px";
+    document.querySelector("body").style.backgroundColor = "#292C35";
+  } else {
+    document.getElementById("circle").style.left = "10px";
+    document.querySelector("body").style.backgroundColor = "#ffffff";
+  }
+
+  switchinp = !switchinp;
+}
+check();
